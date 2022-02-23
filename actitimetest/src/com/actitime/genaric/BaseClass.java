3c99cc46c55b0a437fa226dc09e9657c230899cd
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-02-23 11:07:46 +0530

    new

--- actitimetest/src/com/actitime/genaric/BaseClass.java
+++ actitimetest/src/com/actitime/genaric/BaseClass.java
@@ -1,9 +1,26 @@
 package com.actitime.genaric;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public class BaseClass {
-	
+	@BeforeTest
 	public void openBrowser() {
-		
+
+	}
+	@AfterTest
+	public void closeBrowser() {
+
+	}
+	@BeforeMethod
+	public void logIn() {
+
+	}
+	@AfterMethod
+	public void logOut() {
+
 	}
 
 }
--- actitimetest/src/com/actitime/genaric/FileLib.java
+++ actitimetest/src/com/actitime/genaric/FileLib.java
@@ -1,5 +1,5 @@
 package com.actitime.genaric;
 
 public class FileLib {
-
+	
 }
